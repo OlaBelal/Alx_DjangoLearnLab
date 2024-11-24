f481af1282c2f63b00299ef6d43e84cefbf691fd
OlaBelal <olabelal1995@gmail.com>
2024-11-24 23:38:25 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -50,4 +50,20 @@ class LibraryDetailView(DetailView):
 
 
 
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+# Registration view
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after successful registration
+            return redirect('login')  # Redirect to login page
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
