4eeb6901d6f213cc568c8868276c8d5e1abb142e
OlaBelal <olabelal1995@gmail.com>
2024-11-24 23:38:40 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,3 +8,15 @@ urlpatterns = [
     # Class-based view for library details
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
+from django.urls import path
+from .views import register
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    # Login
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    # Logout
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    # Registration
+    path('register/', register, name='register'),
+]
