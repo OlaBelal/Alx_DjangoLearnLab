87ce8db29d897561f94664115902c10f798e11e2
OlaBelal <olabelal1995@gmail.com>
2024-11-24 23:43:45 +0200

    Update settings.py

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
