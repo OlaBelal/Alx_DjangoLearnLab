9d94c05e4d60f50d3e9b1f578bb6808fea91d176
OlaBelal <olabelal1995@gmail.com>
2024-11-24 23:45:59 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,69 +1,14 @@
-
-# relationship_app/views.py
-from django.views.generic.detail import DetailView
-
-
-from django.views.generic.detail import DetailView
-
-
-from django.shortcuts import render
-from .models import Book
-
-def list_books(request):
-    books = Book.objects.all()  # Retrieve all books from the database
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-# relationship_app/views.py
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Library
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'  # The name of the variable passed to the template
-
-    # Optional: You can override the get_context_data method to add additional context
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Add more data to the context if needed
-        return context
-
-# relationship_app/views.py
-from django.shortcuts import render
-from .models import Book
-
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-# relationship_app/views.py
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Library
-
-# Class-based view for library details
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'  # The template to render
-    context_object_name = 'library'  # Name of the context variable used in the template
-
-
-
-from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
-# Registration view
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log in the user after successful registration
-            return redirect('login')  # Redirect to login page
+            login(request, user)
+            return redirect('login')  # Redirect to login page after successful registration
     else:
         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
-
+    return render(request, 'relationship_app/register.html', {'form': form})  # Ensure template path is correct
