1c3a091a74ba6600452edc82e996bee72c179432
OlaBelal <olabelal1995@gmail.com>
2024-12-01 19:44:58 +0200

    Update urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.contrib import admin
-from django.urls import path, include  # Don't forget to import include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include the API URLs
+    path('api/', include('api.urls')),
 ]
