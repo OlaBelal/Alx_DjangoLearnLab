5c465deb92952105232d4db3ca59e9ae5ab9ed80
OlaBelal <olabelal1995@gmail.com>
2024-12-03 20:58:25 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -43,3 +43,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
