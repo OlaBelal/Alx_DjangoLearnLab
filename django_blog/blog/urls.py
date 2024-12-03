3895395950ac462080053b6f8b64b76258e42a3c
OlaBelal <olabelal1995@gmail.com>
2024-12-03 21:03:07 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -32,6 +32,10 @@ urlpatterns = [
      path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+
+    # Other URLs
+    path('search/', views.search, name='search'),
+    path('tags/<str:tag_name>/', views.tagged_posts, name='tagged_posts'),
 ]
 
 
