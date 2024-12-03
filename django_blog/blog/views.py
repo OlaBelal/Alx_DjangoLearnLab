08a3580a2b114a9c02fa4c36baa5cbea04836b38
OlaBelal <olabelal1995@gmail.com>
2024-12-03 21:00:17 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
+from django.db.models import Q
+from django.shortcuts import render
+from .models import Post
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
