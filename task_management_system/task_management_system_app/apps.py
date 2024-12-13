56034f45402b379a54ee8093f5f0b3c5466017c4
OlaBelal <olabelal1995@gmail.com>
2024-12-13 23:26:34 +0200

    Delete task_management_system directory

--- task_management_system/task_management_system_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaskManagementSystemAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'task_management_system_app'
