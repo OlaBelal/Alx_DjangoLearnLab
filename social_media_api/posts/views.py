df57ce0dc384e92251441ac363ced5be56cd4f26
OlaBelal <olabelal1995@gmail.com>
2024-12-15 15:23:10 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,63 +1,21 @@
-from rest_framework import viewsets, permissions
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from django.contrib.auth import get_user_model
-from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
-
-
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.pagination import PageNumberPagination
+from rest_framework import generics, permissions
 from rest_framework.response import Response
-from django.db.models import Q
+from django.contrib.auth import get_user_model
 from .models import Post
 from .serializers import PostSerializer
 
-# Permissions to ensure users can only edit/delete their own posts and comments
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+# This view will return the posts from users that the current user follows.
+class FeedView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = PostSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
-    filterset_fields = ['title', 'content']
-    ordering_fields = ['created_at']
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    def get_queryset(self):
+        # Get the current authenticated user
+        user = self.request.user
 
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+        # Get the list of users that the current user is following
+        following_users = user.following.all()
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        # Fetch posts from users that the current user is following, ordered by creation date (most recent first)
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
 
-
-class PostPagination(PageNumberPagination):
-    page_size = 5
-    page_size_query_param = 'page_size'
-    max_page_size = 10
-
-class FeedViewSet(viewsets.ViewSet):
-    permission_classes = [IsAuthenticated]
-    pagination_class = PostPagination
-
-    def list(self, request):
-        user = request.user
-        following = user.following.all()  # Get users the current user is following
-        posts = Post.objects.filter(author__in=following).order_by('-created_at')  # Get posts from followed users
-
-        page = self.paginate_queryset(posts)
-        if page is not None:
-            serializer = PostSerializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
