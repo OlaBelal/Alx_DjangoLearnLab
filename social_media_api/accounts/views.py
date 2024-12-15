807f2dc52ec46962ff9781ab3c6481c4b721ccaa
OlaBelal <olabelal1995@gmail.com>
2024-12-15 15:22:03 +0200

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,44 +1,56 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from .models import CustomUser
+from django.contrib.auth import get_user_model
 from .serializers import CustomUserSerializer
-from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
+from .models import CustomUser
 
-# User list and detail views
-class UserListView(generics.ListCreateAPIView):
-    queryset = CustomUser.objects.all()  # All users
-    serializer_class = CustomUserSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can access
+# Create your views here.
 
-class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+class UserListView(generics.ListAPIView):
     queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can access
-
-# Follow and Unfollow Views
-@api_view(['POST'])
-def follow_user(request, user_id):
-    user_to_follow = CustomUser.objects.get(id=user_id)
-    request.user.following.add(user_to_follow)  # Add to the current user's following list
-    return Response({"message": f"You are now following {user_to_follow.username}"})
-
-@api_view(['POST'])
-def unfollow_user(request, user_id):
-    user_to_unfollow = CustomUser.objects.get(id=user_id)
-    request.user.following.remove(user_to_unfollow)  # Remove from following list
-    return Response({"message": f"You have unfollowed {user_to_unfollow.username}"})
-
-# Feed View - Posts from followed users
-class FeedView(generics.ListAPIView):
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user = request.user
+        try:
+            follow_user = CustomUser.objects.get(id=user_id)
+        except CustomUser.DoesNotExist:
+            return Response({"detail": "User not found"}, status=404)
+
+        user.following.add(follow_user)
+        return Response({"detail": "Following user"}, status=200)
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user = request.user
+        try:
+            unfollow_user = CustomUser.objects.get(id=user_id)
+        except CustomUser.DoesNotExist:
+            return Response({"detail": "User not found"}, status=404)
+
+        user.following.remove(unfollow_user)
+        return Response({"detail": "Unfollowed user"}, status=200)
+
+# To get the list of users a person is following
+class FollowingListView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def get_queryset(self):
-        # Get the posts from followed users
         user = self.request.user
-        followed_users = user.following.all()  # Get the users the current user is following
-        return followed_users.posts.all()  # Assuming that posts are linked with the user
+        return user.following.all()
+
+# To get the list of followers of a user
+class FollowersListView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.followers_set.all()
 
