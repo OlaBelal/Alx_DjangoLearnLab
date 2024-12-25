f58831e86e49f92e092265ee44eb030c1221e003
OlaBelal <olabelal1995@gmail.com>
2024-12-25 14:38:37 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
+from django.contrib.auth import login
 
 def list_books(request):
     books = Book.objects.all()
@@ -17,18 +18,20 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
     
+
 # Custom registration view
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            user = form.save()  # Save the user to the database
+            login(request, user)  # Log in the user immediately after registration
+            return redirect('dashboard')  # Redirect to the dashboard
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
-# Optional: A simple dashboard for authenticated users
 @login_required
 def dashboard(request):
     return render(request, 'relationship_app/dashboard.html')
+ 
